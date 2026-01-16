ac505035945e734ffc6cd41786aa1cb6f8d22553
MD Shimul Hossen <mdshimulhossen119@gmail.com>
2026-01-16 19:24:03 +0600

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -41,7 +41,7 @@ const Services = () => {
               </h1>
             </div>
             {/* Services card section */}
-<a hrf="https://www.amazon.com/Lifeboost-Coffee-Espresso-Ground-Mycotoxins/dp/B09V98JGJ8/ref=sr_1_8?crid=1F4T24H202504&dib=eyJ2IjoiMSJ9.lQ0ffOLdMyM0r2FBMM7JxDykEf-iOOtkQJLl9kDpjNIDREtiYJVrud2-6JKF2dpTUmqDrth0HoLHJb8BYqrgQ_EEEPawdP6vZLhgw0brPE-dRDGIV5xZHyVmkAY2ZeNdSOrGrju-KxE2n3GxqQQZG8SsFXNutOD61P36apTJi5q6af4qm7YUydpCFjwLftXhg83TAw3WnZK1oiwiYjZbHBs2TZFkAYQdfTEUGoceSRUxvqRGUYMB35_6_vCYS1yClOzgRKXYTMXFJtFTIFlk92_9w07TiVXi6XN3JLLr8Ww.zCsea6A2Ce77QDocgjuvB17ZtsYUqzeQdxzoRn--aU0&dib_tag=se&keywords=americano%2Bcoffee&qid=1768569151&sprefix=amercano%2Caps%2C462&sr=8-8&th=1"
+<a href="https://www.amazon.com/Lifeboost-Coffee-Espresso-Ground-Mycotoxins/dp/B09V98JGJ8/ref=sr_1_8?crid=1F4T24H202504&dib=eyJ2IjoiMSJ9.lQ0ffOLdMyM0r2FBMM7JxDykEf-iOOtkQJLl9kDpjNIDREtiYJVrud2-6JKF2dpTUmqDrth0HoLHJb8BYqrgQ_EEEPawdP6vZLhgw0brPE-dRDGIV5xZHyVmkAY2ZeNdSOrGrju-KxE2n3GxqQQZG8SsFXNutOD61P36apTJi5q6af4qm7YUydpCFjwLftXhg83TAw3WnZK1oiwiYjZbHBs2TZFkAYQdfTEUGoceSRUxvqRGUYMB35_6_vCYS1yClOzgRKXYTMXFJtFTIFlk92_9w07TiVXi6XN3JLLr8Ww.zCsea6A2Ce77QDocgjuvB17ZtsYUqzeQdxzoRn--aU0&dib_tag=se&keywords=americano%2Bcoffee&qid=1768569151&sprefix=amercano%2Caps%2C462&sr=8-8&th=1"
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center">
               {ServicesData.map((data, index) => {
                 return (
